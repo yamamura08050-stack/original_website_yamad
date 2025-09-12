5cc9852e7fc9341408584c1d0e147f74aac36021
WataruOhkura <wohkura@nexseed.net>
2025-09-12 13:44:15 +0800

    save

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    
+
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -74,6 +74,10 @@
                 </div>
             </div>
         </nav>
+        <div>
+            <h1>hello
+            </h1>
+        </div>
 
         <main class="py-4">
             @yield('content')
